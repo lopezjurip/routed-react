3fff369b0319c0a1a3741a0b15ed02beaac1a539
Patricio López <lopezjuripatricio@gmail.com>
2016-08-23 16:38:09 -0300

    Use Supertest as promise instead of callbacks

--- test/server.test.js
+++ test/server.test.js
@@ -1,8 +1,6 @@
 // test/server.test.js
-'use strict';
-
 const exec = require('mz/child_process').exec;
-const request = require('supertest');
+const request = require('supertest-as-promised');
 const expect = require('chai').expect;
 
 const app = require('../server/app');
@@ -18,35 +16,26 @@ describe('builds application', function () {
 });
 
 describe('express serving', function () {
-  it('responds to / with the index.html', function (done) {
-    request(app)
+  it('responds to / with the index.html', function () {
+    return request(app)
       .get('/')
       .expect('Content-Type', /html/)
       .expect(200)
-      .end((err, res) => {
-        if (err) done(err);
-        expect(res.text).to.contain('<div id="root"></div>');
-        done();
-      });
+      .then(res => expect(res.text).to.contain('<div id="root"></div>'));
   });
 
-  it('responds to favicon.icon request', function (done) {
-    request(app)
+  it('responds to favicon.icon request', function () {
+    return request(app)
       .get('/favicon.ico')
       .expect('Content-Type', 'image/x-icon')
-      .expect(200)
-      .end(done);
+      .expect(200);
   });
 
-  it('responds to any route with the index.html', function (done) {
-    request(app)
+  it('responds to any route with the index.html', function () {
+    return request(app)
       .get('/foo/bar')
       .expect('Content-Type', /html/)
       .expect(200)
-      .end((err, res) => {
-        if (err) done(err);
-        expect(res.text).to.contain('<div id="root"></div>');
-        done();
-      });
+      .then(res => expect(res.text).to.contain('<div id="root"></div>'));
   });
 });
