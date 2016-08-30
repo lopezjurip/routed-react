a40ebe26bd7707fe517749274496972b78695524
Patricio López <lopezjuripatricio@gmail.com>
2016-08-30 08:53:40 -0300

    Allow better composability

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,19 @@
 // src/components/App/index.js
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
 import classnames from 'classnames';
 
 import logo from './logo.svg';
 import './style.css';
 
 class App extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-    style: PropTypes.object,
-  }
+  static propTypes = {}
+  static defaultProps = {}
+  state = {}
 
   render() {
+    const { className, ...props } = this.props;
     return (
-      <div className={classnames('App', this.props.className)} style={this.props.style}>
+      <div className={classnames('App', className)} {...props}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,18 +1,18 @@
 // src/components/NotFound/index.js
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
 import classnames from 'classnames';
 
 import './style.css';
 
 export default class NotFound extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-    style: PropTypes.object,
-  }
+  static propTypes = {}
+  static defaultProps = {}
+  state = {}
 
   render() {
+    const { className, ...props } = this.props;
     return (
-      <div className={classnames('NotFound', this.props.className)} style={this.props.style}>
+      <div className={classnames('NotFound', className)} {...props}>
         <h1>
           404 <small>Not Found :(</small>
         </h1>
